5376bcefa7a34c45b7a5dd19ef7f49b1b272af3f
Arlania <liemilyrong@gmail.com>
2026-07-31 22:22:19 +0800

    删除我的，加入profile icon

--- components/museum-nav/museum-nav.js
+++ components/museum-nav/museum-nav.js
@@ -9,8 +9,7 @@ Component({
     items: [
       { key: 'home', label: '主页', icon: '馆', url: '/pages/index/index' },
       { key: 'gallery', label: '展厅', icon: '展', url: '/pages/gallery/gallery' },
-      { key: 'summary', label: '总结', icon: '总', url: '/pages/summary/summary' },
-      { key: 'profile', label: '我的', icon: '我', url: '/pages/profile/profile' }
+      { key: 'summary', label: '总结', icon: '总', url: '/pages/summary/summary' }
     ]
   }
 })
